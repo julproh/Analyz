4c4303ece3fb2c5a42d3580ca35517687c49d430
julproh <y.prokhorova>
2024-06-05 12:09:32 +0300

    Проект по анализу данных

---
+++ csv_to_html.py
@@ -0,0 +1,20 @@
+import csv
+
+def csv_to_html(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    html = "<table class="table">\n"
+    for row in data:
+        html += "  <tr>\n"
+        for cell in row:
+            html += f"    <td>{cell}</td>\n"
+        html += "  </tr>\n"
+    html += "</table>"
+
+    return html
+
+csv_file = 'dog_breeds.csv'
+html_table = csv_to_html(csv_file)
+print(html_table)
